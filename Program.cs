7e4dc11f8f557bda74f35d83e237359996201f5e
lucas <lucas.r.s.s.2031@gmail.com>
2026-06-11 20:24:06 -0300

    feat: cria classe multiplicacao com metodo de calculo

--- Program.cs
+++ Program.cs
@@ -2,6 +2,8 @@
 soma somar = new soma();
 subitracao subitracao = new subitracao();
 divisao divisao = new divisao();
+multiplicacao multiplicacao = new multiplicacao();
 Console.WriteLine(somar.calcular(3, 5));
 Console.WriteLine(subitracao.calcularSub(3, 5));
 Console.WriteLine(divisao.calcularDiv(3,5));
+Console.WriteLine(multiplicacao.CalcularMult(3,5));
---
+++ mutiplicacao.cs
@@ -0,0 +1,7 @@
+﻿public class multiplicacao
+{
+    public double CalcularMult(double num1, double num2)
+    {
+        return num1 * num2;
+    }
+}
