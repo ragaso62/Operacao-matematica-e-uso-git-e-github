061114513ff138e37acad6bc5c1fcd3efe1c0709
unknown <Administrador@CORLAB176673.senac.educ>
2026-06-10 21:22:52 -0300

    feat: arquivo subritracao.cs criado e com metodo de calculo, testado no Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
 ﻿Console.WriteLine("Hello, World!");
 soma somar = new soma();
+subitracao subitracao = new subitracao();
 Console.WriteLine(somar.calcular(3, 5));
+Console.WriteLine(subitracao.calcularSub(3, 5));
---
+++ classe-subtracao.cs
@@ -0,0 +1,7 @@
+﻿public class subitracao
+{
+    public double calcularSub(double num1, double num2)
+    {
+        return num1 - num2;
+    }
+}
